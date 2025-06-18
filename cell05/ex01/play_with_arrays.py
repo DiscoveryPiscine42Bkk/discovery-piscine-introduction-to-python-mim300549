ff31ac908031c9612648db1f4623f8eec335a8ec
mim300549 <yayanipon@gmail.com>
2025-06-18 04:02:51 +0000

    42

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,11 @@
+
+
+orininal_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+
+new_array = [num + 2 for num in oringinal_array]
+
+
+print("Original array:", original_array)
+print("Num array:", new_array)
+
