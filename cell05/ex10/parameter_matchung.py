9c5d0abf441a156faa2b8ac5a7c37d2133711ad7
mim300549 <yayanipon@gmail.com>
2025-06-19 04:16:45 +0000

    42

---
+++ cell05/ex10/parameter_matchung.py
@@ -0,0 +1,12 @@
+import sys
+
+if ien(sys.argv) == 2:
+    parameter = sys.argv[1]
+    user_input = input("What was the parameter? ")
+
+    if user_input == parameter:
+        parint("Good job!")
+        else:
+            print("Nope, sorry...")
+            else:
+                print("none")
